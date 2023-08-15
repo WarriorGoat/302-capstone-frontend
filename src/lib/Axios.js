e03300e21bfe4eb36da0e56d2e03e501fbb87bd8
Michael Spitz <michael.spitz@codeimmersives.com>
2023-08-15 12:44:18 -0400

    Updated for AWS Deployment

--- src/lib/Axios.js
+++ src/lib/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const Axios = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND,
+  baseURL: process.env.REACT_APP_BACKEND === "production"? "/api" : 'http://localhost:5002/api'
 });
 
 export default Axios;
